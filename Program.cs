73a72b7fc9d45bd054707e2e5e6c789584318df2
Vincent Xu <vincent.xu1@outlook.com>
2025-07-16 14:20:55 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Assignment_2._4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the array size: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] nums = new int[size];
+
+            for(int i=0; i<size; i++)
+            {
+                Console.Write($"Input {i+1}{DetermineOrdinal(i+1)} number: ");
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            (int index, int value) result = FindMax(nums);
+            Console.WriteLine($"The largest number in the array is the {result.index + 1}{DetermineOrdinal(result.index)} number '{result.value}' at index {result.index}.");
+        }
+        // For an int[] argument of non-zero size, return the max value and its index.
+        static (int index, int value) FindMax(int[] nums)
+        {
+            // For an argument with no elements, return the following.
+            int index = -1, max = int.MinValue;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > max)
+                {
+                    max = nums[i];
+                    index = i;
+                }
+            }
+            return (index, max);
+        }
+        static string DetermineOrdinal(int num)
+        {
+            if (num != 13 && num % 10 == 3)
+                return "rd";
+            else if (num != 12 && num % 10 == 2)
+                return "nd";
+            else if (num != 11 && num % 10 == 1)
+                return "st";
+            else
+                return "th";
+        }
+    }
+}
